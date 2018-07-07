28b55eb0fa3a7b9cfd5e371959070d04f26693c6
Polina Tumanian <>
2018-07-07 16:18:28 +0300

    changed tournament

--- hw6-TumanyanPolina.js
+++ hw6-TumanyanPolina.js
@@ -1,31 +1,3 @@
-Hero.TYPE_THIEF = 'thief';
-Hero.TYPE_WARS = 'wars';
-Hero.TYPE_WIZARD = 'wizard';
-Monster.TYPE_GOBLIN = 'goblin';
-Monster.TYPE_ORCS = 'orks';
-Monster.TYPE_VAMPIRE = 'vampire';
-
-Hero.TYPE_LIFE_MAP = new Map([
-    [Hero.TYPE_THIEF, 200],
-    [Hero.TYPE_WARS, 250],
-    [Hero.TYPE_WIZARD, 180],
-]);
-Hero.TYPE_DAMAGE_MAP = new Map([
-    [Hero.TYPE_THIEF, 40],
-    [Hero.TYPE_WARS, 20],
-    [Hero.TYPE_WIZARD, 60],
-]);
-Monster.TYPE_LIFE_MAP = new Map([
-    [Monster.TYPE_GOBLIN, 170],
-    [Monster.TYPE_ORCS, 300],
-    [Monster.TYPE_VAMPIRE, 150]
-]);
-Monster.TYPE_DAMAGE_MAP = new Map([
-    [Monster.TYPE_GOBLIN, 55],
-    [Monster.TYPE_ORCS, 15],
-    [Monster.TYPE_VAMPIRE, 80]
-]);
-
 class Character {
     costructor(life, damage) {
         this.life = life;
@@ -90,25 +62,74 @@ class Character {
 }
 
 class Hero extends Character {
-    constructor(type) {
+    constructor(type, name) {
         const life = Hero.TYPE_LIFE_MAP.get(type);
         const damage = Hero.TYPE_DAMAGE_MAP.get(type);
         super(life, damage);
         this.type = type;
+        if (this.checkName(name)) {
+            this.name = name;
+        } else {
+            throw new Error('This name not found! :(');
+        }
+    }
+    checkName(name) {
+        if (Hero.NAME.includes(name));
+        else return false;
     }
 }
 
 class Monster extends Character {
-    constructor(type) {
+    constructor(type, name) {
         const life = Monster.TYPE_LIFE_MAP.get(type);
         const damage = Monster.TYPE_DAMAGE_MAP.get(type);
         super(life, damage);
         this.type = type;
+        if (this.checkName(name)) {
+            this.name = name;
+        } else {
+            throw new Error('This name not found! :(');
+        }
+    }
+    checkName(name) {
+        if (Monster.NAME.includes(name));
+        else return false;
     }
 }
 
+Hero.TYPE_THIEF = 'thief';
+Hero.TYPE_WARS = 'wars';
+Hero.TYPE_WIZARD = 'wizard';
+Hero.NAME = ['Maxim', 'Kirill', 'Guldan', 'Naymar', 'Ronaldo', 'Jesus', 'Leonid'];
+Monster.TYPE_GOBLIN = 'goblin';
+Monster.TYPE_ORCS = 'orks';
+Monster.TYPE_VAMPIRE = 'vampire';
+Monster.NAME = ['Polina', 'Anna', 'Olga', 'Galina', 'Kompany', 'Mumu', 'Ququsha'];
+
+
+Hero.TYPE_LIFE_MAP = new Map([
+    [Hero.TYPE_THIEF, 200],
+    [Hero.TYPE_WARS, 250],
+    [Hero.TYPE_WIZARD, 180],
+]);
+Hero.TYPE_DAMAGE_MAP = new Map([
+    [Hero.TYPE_THIEF, 40],
+    [Hero.TYPE_WARS, 20],
+    [Hero.TYPE_WIZARD, 60],
+]);
+Monster.TYPE_LIFE_MAP = new Map([
+    [Monster.TYPE_GOBLIN, 170],
+    [Monster.TYPE_ORCS, 300],
+    [Monster.TYPE_VAMPIRE, 150]
+]);
+Monster.TYPE_DAMAGE_MAP = new Map([
+    [Monster.TYPE_GOBLIN, 55],
+    [Monster.TYPE_ORCS, 15],
+    [Monster.TYPE_VAMPIRE, 80]
+]);
+
 class Tournament {
-    constructor(n, characters) { //передаем количество участников и самих участников
+    constructor(n, characters) { //передаем количество участников и самих участников 
         this.verifyQuantity(n, characters);
         this.n = n;
     }
@@ -119,15 +140,56 @@ class Tournament {
             throw new Error('Error!!!Too many participants registered!!! ');
     }
     start() {
-        battle() {
+        return this.battle(this.chatacters);
+    }
 
+    battle(characters) {
+        if (characters.length > 2) {
+            const char1 = this.battle(characters.slice(0, characters.length / 2));
+            const char2 = this.battle(characters.slice(characters.length / 2, characters.length));
+            return this.fight(char1, char2);
+        } else if (characters.length > 1) {
+            return this.fight(characters[0], characters[1]);
+        } else {
+            return characters[0];
         }
+    }
+
+    fight(char1, char2) {
+        if (char1 === null) {
+            if (char2 === null) {
+                return null;
+            }
+            return char2;
+        }
+        if (char2 === null) {
+            return char1;
+        }
+        while (char1.isAlive && char2.isAlive) {
+            char1.attack(char2);
+            console.log(`${char1.name} take damage ${char2.name}${char1.damage}! ${char2.name} has ${char2.life} lives left`);
+            char2.attack(char1);
+            console.log(`${char2.name} take damage ${char1.name}${char2.damage}! ${char1.name} has ${char1.life} lives left`);
+        }
+        let winner;
+        if (char1.isAlive) {
+            winner = char1;
+            char1.life = char1.maxLife;
+        }
+        if (char2.isAlive) {
+            winner = char2;
+            char2.life = char2.maxLife;
+        } else {
+            return null;
+        }
+        return winner;
     }
 }
 
+let tourney = new Tournament(3, [
+    new Hero(Hero.TYPE_THIEF, 'Leonid'),
+    new Hero(Hero.TYPE_WIZARD, 'Naymar'),
+    new Hero(Hero.TYPE_WARS, 'Guldan')
+]);
 
-
-
-let monster1 = new Monster(Monster.TYPE_ORCS);
-let hero1 = new Hero(Hero.TYPE_THIEF);
-let characters = [monster1, hero1]
+console.log(tourney.start());
